7b36f865ae50c806a33cbe1cae4d82165e883da5
jorundur13 <jorundur13@skel.ru.is>
2015-10-30 20:24:03 +0000

    flot

---
+++ scripts/server_run3.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./src/chatd $(/labs/tsam15/my_port) /home/hir.is/jorundur13/Tolvusamskipti/tsam15/pa3/src/fd.crt /home/hir.is/jorundur13/Tolvusamskipti/tsam15/pa3/src/fd.key /home/hir.is/jorundur13/Tolvusamskipti/tsam15/pa3/src/CAfile.pem  
