00c5d2f613c9317430d3ac9ab4cbc4b8de95d2d1
Daniel Benediktsson <danielb13@ru.is>
2015-10-31 22:50:17 +0000

    flot

--- src/chatd.c
+++ src/chatd.c
@@ -59,7 +59,6 @@ gboolean fd_set_nodes(gpointer key, gpointer value, gpointer data) {
 gboolean is_greater_fd(gpointer key, gpointer value, gpointer data) {
     struct connection *conn = (struct connection *) value;
     int fd = *(int *) data;
-
     if(conn->connfd > fd) {
         *(int *)data = conn->connfd;
     }
@@ -88,7 +87,6 @@ gboolean check_connection(gpointer key, gpointer value, gpointer data) {
     fd_set *rfds = (fd_set *) data;
     char recvMessage[MAX_LENGTH];
     int sizerly = 0;
-    
     if(conn->connfd != -1){
         if(FD_ISSET(conn->connfd, rfds)){
             memset(recvMessage, '\0', strlen(recvMessage));
@@ -235,12 +233,8 @@ int main(int argc, char **argv) {
 
         FD_ZERO(&rfds);
 
-        fprintf(stdout, "before - highestFD : %d\n", highestFD);
-        fflush(stdout);
         g_tree_foreach(tree, is_greater_fd, &highestFD);
         g_tree_foreach(tree, fd_set_nodes, &rfds);
-        fprintf(stdout, "after - highestFD : %d\n", highestFD);
-        fflush(stdout);
         
         FD_SET(listen_sock, &rfds);
         if(listen_sock > highestFD) {
