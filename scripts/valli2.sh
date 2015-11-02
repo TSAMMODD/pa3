b7041f6377375f0e6a2aa326562a7f48bcb57ec0
K371 <thorkell@me.com>
2015-11-02 16:33:47 +0000

    flot

---
+++ scripts/valli2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+valgrind --tool=memcheck src/chatd $(/labs/tsam15/my_port) /home/hir.is/thorkell12/pa3/src/fd.crt /home/hir.is/thorkell12/pa3/src/fd.key /home/hir.is/thorkell12/pa3/src/CAfile.pem
+
