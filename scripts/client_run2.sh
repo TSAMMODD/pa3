83a6b26f466388572f9b24c511643d4f31572c19
K371 <thorkell@me.com>
2015-11-03 21:00:06 +0000

    flot

--- scripts/client_run2.sh
+++ scripts/client_run2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./src/chat localhost $(/labs/tsam15/my_port) /home/hir.is//pa3/src/fd.crt /home/hir.is/thorkell12/pa3/src/fd.key /home/hir.is/thorkell12/pa3/src/CAfile.pem
+./src/chat $(/labs/tsam15/my_port) /home/hir.is/thorkell12/pa3/src/CAfile.pem
