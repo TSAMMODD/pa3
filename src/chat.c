7f68c8ffbed231b900f1335d19e004e476de8dfa
Daniel Benediktsson <danielb13@ru.is>
2015-11-03 16:28:07 +0000

    flot

--- src/chat.c
+++ src/chat.c
@@ -197,11 +197,6 @@ void readline_callback(char *line) {
             rl_redisplay();
             return;
         }
-        /* Start game */
-        if(SSL_write(server_ssl, line, strlen(line)) < 0 ){
-            perror("Error Writing /game to server\n");
-            exit(1);
-        }
         return;
     }
     if (strncmp("/join", line, 5) == 0) {
